afe80b4ddf47092d74b5655f3df7523de71d08cc
Tom <tsiener@gmx.de>
2017-11-16 00:17:59 +0100

    site.mk - v028 mit gluon2017.1.4

--- site.mk
+++ site.mk
@@ -203,7 +203,7 @@ endif
 #                       opkg compare-versions "$1" '>>' "$2"
 #               to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := ffso-v027m
+DEFAULT_GLUON_RELEASE := ffso-v028
 
 #       GLUON_RELEASE
 #               call make with custom GLUON_RELEASE flag, to use your own release version scheme.
